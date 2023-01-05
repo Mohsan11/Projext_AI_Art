d38960b2a288b309cf585bdc05275e0cc100a2d7
Mohsan Ali <mohsanali1132@gmail.com>
2023-01-06 01:13:25 +0500

    Project completed successfully.

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const Port = process.env.PORT || 5000;
 const app = express();
+const path = require("path");
+
 //Enable body parser
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: false }));
+
+//Set Static Folder
+app.use(express.static(path.join(__dirname, "Public")));
+
 app.use("/openai", require("./routes/openaiRoutes"));
 app.listen(Port, () => {
   console.log(`Server started on port: ${Port}`);
